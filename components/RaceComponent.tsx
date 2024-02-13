4de81247a78943fd80eaf1d19a481726957b75c7
Max Pylypenko <73674241+3mpee3mpee@users.noreply.github.com>
2024-02-14 00:34:02 +0100

    added charIndex + wordIndexing + few keycodes cases

--- components/RaceComponent.tsx
+++ components/RaceComponent.tsx
@@ -9,16 +9,31 @@ type Props = {
 export function RaceComponent(props: Props) {
     const textString: String = props.text;
     const wordsArr: string[] = textString.split(' ');
-    const [currWordIndex, setCurrWordIndex] = useState<Number>(0);
-    const [currCharIndex, setCurrCharIndex] = useState<Number>(0);
+    // const [currWordIndex, setCurrWordIndex] = useState<Number>(0);
+    const [currCharIndex, setCurrCharIndex] = useState<number>(0);
     const [textInput, setTextInput] = useState<string>('');
 
     const inputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
-        const handleKeyPress = () => {
-            console.log('OK');
-            console.log(textInput);
+        const handleKeyPress = (e: KeyboardEvent) => {
+            const hiddenInputElement: HTMLElement | null = document.querySelector('.hidden-input');
+            const textInputArr: string[] = textInput.split(' ');
+            const wordIndex: number = textInputArr.length;
+            let charIndex: number = 0;
+
+            if (e.key === 'Delete' || e.key === 'Backspace') {
+                charIndex = currCharIndex && currCharIndex - 1 || 1;
+            } else if (e.code === 'Space') {
+                charIndex = 0;
+            } else {
+                charIndex = currCharIndex && currCharIndex + 1 || 1;
+            }
+
+            
+            const wordElem = document.querySelector(`[data-word-index="${wordIndex}"]`);
+            
+            setCurrCharIndex(charIndex);
         };
 
         // Add event listener for keypress
@@ -42,20 +57,21 @@ export function RaceComponent(props: Props) {
             }
             clearInterval(intervalId);
         };
-    }, [textInput]);
+    }, [textInput, currCharIndex, wordsArr]);
 
     return (
         <div className="sm:px-12 md:px-32 lg:px-52 text-xl tracking-wider text-gray-400">
             <div className="flex flex-wrap">
                 <input
-                    type="text"
+                    className='hidden-input'
+                    type='text'
                     value={textInput}
                     ref={inputRef}
                     onInput={(e: React.FormEvent<HTMLInputElement>) => setTextInput(e.currentTarget.value)}
                     style={{ position: 'fixed', top: '-9999px', left: '-9999px' }}
                 />
 
-                {wordsArr.map((word, i) => <div className='flex' key={i} data-word-index={i}>{word.split('').map((char, i) => <div key={i} data-char-index={i}>{char}</div>)} &nbsp;</div>)}
+                {wordsArr.map((word, i) => <div className='flex' key={i} data-word-index={i + 1}>{word.split('').map((char, i) => <div key={i} data-char-index={i + 1}>{char}</div>)} &nbsp;</div>)}
             </div>
         </div>
     );
